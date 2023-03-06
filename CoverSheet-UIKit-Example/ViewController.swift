e22e20b502557eb81bbc20b6e1191248d3c9f580
Colin Walsh <colinfwalsh@gmail.com>
2023-03-06 14:21:52 -0500

    First commit

---
+++ CoverSheet-UIKit-Example/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  CoverSheet-UIKit-Example
+//
+//  Created by Colin Walsh on 3/6/23.
+//
+
+import UIKit
+import CoverSheet
+
+class ViewController: CoverSheetController {
+    
+    let innerVC = UIViewController()
+    let sheetVC = UIViewController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configure(inner: innerVC, sheet: sheetVC)
+        updateSheet(shouldBlur: false, backgroundColor: .white)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        innerVC.view.backgroundColor = .gray
+    }
+
+}
+
